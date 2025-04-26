9b301e059108edc46f37edbb83c5a60e00bb60f4
Mohd Aarif <aarifmohd799@gmail.com>
2025-04-26 10:16:03 +0530

    Update API endpoints to use Render backend

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,4 @@
-export const USER_API_END_POINT="http://localhost:8000/api/v1/users"
-export const JOB_API_END_POINT="http://localhost:8000/api/v1/job"
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application"
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/companies"
-
+export const USER_API_END_POINT = "https://jobportal-backend.onrender.com/api/v1/users";
+export const JOB_API_END_POINT = "https://jobportal-backend.onrender.com/api/v1/job";
+export const APPLICATION_API_END_POINT = "https://jobportal-backend.onrender.com/api/v1/application";
+export const COMPANY_API_END_POINT = "https://jobportal-backend.onrender.com/api/v1/companies";
