aeb8b2c6bdd558889eee70761d64db08b892be80
Mohd Aarif <aarifmohd799@gmail.com>
2025-04-27 10:47:43 +0530

    push updated code

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT="https://quickhire-web-5.onrender.com/api/v1/users"
+export const USER_API_END_POINT="https://jobportal-backend.onrender.com/api/v1/users"
 export const JOB_API_END_POINT="https://quickhire-web-5.onrender.com/api/v1/job"
 export const APPLICATION_API_END_POINT="https://quickhire-web-5.onrender.com/api/v1/application"
 export const COMPANY_API_END_POINT="https://quickhire-web-5.onrender.com/api/v1/companies"
